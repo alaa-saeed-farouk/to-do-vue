e9ecc98d7c7962567791f88e4bb683cc64950ffe
alaa-saeed-farouk <alaa.s.farouk19@gmail.com>
2025-01-03 22:09:13 +0200

    Adding the main structure

---
+++ js/848.84c6a64f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkto_do_vue"]=self["webpackChunkto_do_vue"]||[]).push([[848],{3848:function(n,t,o){o.r(t),o.d(t,{default:function(){return c}});var u=o(6768);const e={class:"not-found"};function r(n,t){return(0,u.uX)(),(0,u.CE)("div",e,t[0]||(t[0]=[(0,u.Lk)("h1",{style:{"font-size":"100px",color:"red",margin:"auto"}}," Page Not Found ",-1),(0,u.Lk)("h2",null,"This Path is Not Existed in Our Application",-1)]))}var s=o(1241);const i={},a=(0,s.A)(i,[["render",r]]);var c=a}}]);
+//# sourceMappingURL=848.84c6a64f.js.map
